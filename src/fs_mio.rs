1ae2ce62a61468b86178b96801dcc030dcda53e6
Hexilee <hexileee@gmail.com>
2018-12-07 22:28:22 +0800

    ready to complete fs_mio

--- src/fs_mio.rs
+++ src/fs_mio.rs
@@ -1,138 +1,115 @@
-//use std::sync::mpsc::{Sender, channel};
-//use std::time::Duration;
-//use std::fs::File;
-//use std::io::Read;
-//use std::boxed::FnBox;
-//use mio::*;
-//
-//const FS_TOKEN: Token = Token(9);
-//
-//#[derive(Clone)]
-//pub struct Fs {
-//    task_sender: Sender<Task>,
-//}
-//
-//impl Fs {
-//    pub fn new() -> Self {
-//        let (task_sender, task_receiver) = channel();
-//        let (result_sender, result_receiver) = channel();
-//        let poll = Poll::new().unwrap();
-//        let (register, readiness) = Registration::new2();
-//        poll.register(&register, FS_TOKEN, Ready::readable(), PollOpt::edge()).unwrap();
-//
-//        std::thread::spawn(move || {
-//            let mut events = Events::with_capacity(1024);
-//            loop {
-//                poll.poll(&mut events, None).unwrap();
-//                for event in events.iter() {
-//                    println!("loop event: {:?}", event.token());
-//                    match event.token() {
-//                        FS_TOKEN => {
-//                            println!("fs event");
-//                            match result_receiver.recv_timeout(Duration::from_millis(100)) {
-//                                Ok(result) => {
-//                                    match result {
-//                                        TaskResult::ReadToString(value, callback) => callback(value),
-//                                        TaskResult::Open(file, callback) => callback(file),
-//                                        TaskResult::Exit => return
-//                                    }
-//                                }
-//                                Err(_) => {
-//                                    break;
-//                                }
-//                            }
-//                        }
-//                        _ => unreachable!()
-//                    }
-//                }
-//            }
-//        });
-//
-//        std::thread::spawn(move || {
-//            loop {
-//                match task_receiver.recv() {
-//                    Ok(task) => {
-//                        match task {
-//                            Task::Println(ref string) => println!("{}", string),
-//                            Task::Open(path, callback) => {
-//                                println!("recv task open {}", &path);
-//                                result_sender
-//                                    .clone()
-//                                    .send(TaskResult::Open(File::open(path).unwrap(), callback))
-//                                    .unwrap();
-//                                readiness.set_readiness(Ready::readable()).unwrap();
-//                            }
-//                            Task::ReadToString(mut file, callback) => {
-//                                let mut value = String::new();
-//                                file.read_to_string(&mut value).unwrap();
-//                                result_sender
-//                                    .clone()
-//                                    .send(TaskResult::ReadToString(value, callback))
-//                                    .unwrap();
-//                                readiness.set_readiness(Ready::readable()).unwrap();
-//                            }
-//                            Task::Exit => {
-//                                result_sender
-//                                    .clone()
-//                                    .send(TaskResult::Exit)
-//                                    .unwrap();
-//                                return;
-//                            }
-//                        }
-//                    }
-//                    Err(_) => {
-//                        return;
-//                    }
-//                }
-//            }
-//        });
-//
-//        Fs { task_sender }
-//    }
-//
-//    pub fn println(&self, string: String) {
-//        self.task_sender.send(Task::Println(string)).unwrap()
-//    }
-//
-//    pub fn open(&self, path: &str, callback: FileCallback) {
-//        self.task_sender.send(Task::Open(path.to_string(), callback)).unwrap()
-//    }
-//
-//    pub fn read_to_string(&self, file: File, callback: StringCallback) {
-//        self.task_sender.send(Task::ReadToString(file, callback)).unwrap()
-//    }
-//
-//    pub fn close(&self) {
-//        self.task_sender.send(Task::Exit).unwrap()
-//    }
-//}
-//
-//type FileCallback = Box<FnBox(File) + Send>;
-//type StringCallback = Box<FnBox(String) + Send>;
-//
-//pub enum Task {
-//    Exit,
-//    Println(String),
-//    Open(String, FileCallback),
-//    ReadToString(File, StringCallback),
-//}
-//
-//pub enum TaskResult {
-//    Exit,
-//    Open(File, FileCallback),
-//    ReadToString(String, StringCallback),
-//}
-//
-//const TEST_FILE_VALUE: &str = "Hello, World!";
-//
-//#[test]
-//fn test_fs() {
-//    let fs = Fs::new();
-//    fs.clone().open("./src/test.txt", Box::new(move |file| {
-//        fs.clone().read_to_string(file, Box::new(move |value| {
-//            assert_eq!(TEST_FILE_VALUE, &value);
-//            fs.clone().println(value);
-//            fs.close();
-//        }))
-//    }));
-//}
+use crossbeam_channel::{unbounded, Sender};
+use std::fs::File;
+use std::io::Read;
+use std::boxed::FnBox;
+use mio::*;
+
+#[derive(Clone)]
+pub struct Fs {
+    task_sender: Sender<Task>,
+}
+
+impl Fs {
+    pub fn new() -> Self {
+        let (task_sender, task_receiver) = unbounded();
+        let (result_sender, result_receiver) = unbounded();
+
+        std::thread::spawn(move || {
+            loop {
+                match task_receiver.recv() {
+                    Ok(task) => {
+                        match task {
+                            Task::Println(ref string) => println!("{}", string),
+                            Task::Open(path, callback, fs) => {
+                                result_sender
+                                    .send(TaskResult::Open(File::open(path).unwrap(), callback, fs))
+                                    .unwrap();
+                            }
+                            Task::ReadToString(mut file, callback, fs) => {
+                                let mut value = String::new();
+                                file.read_to_string(&mut value).unwrap();
+                                result_sender
+                                    .send(TaskResult::ReadToString(value, callback, fs))
+                                    .unwrap()
+                            }
+                            Task::Exit => {
+                                result_sender
+                                    .send(TaskResult::Exit)
+                                    .unwrap();
+                                return;
+                            }
+                        }
+                    }
+                    Err(_) => {
+                        return;
+                    }
+                }
+            }
+        });
+
+        std::thread::spawn(move || {
+            loop {
+                match result_receiver.recv() {
+                    Ok(result) => {
+                        match result {
+                            TaskResult::ReadToString(value, callback, fs) => callback(value, fs),
+                            TaskResult::Open(file, callback, fs) => callback(file, fs),
+                            TaskResult::Exit => return
+                        }
+                    }
+                    Err(_) => {
+                        return;
+                    }
+                }
+            }
+        });
+
+        Fs { task_sender }
+    }
+
+    pub fn println(&self, string: String) {
+        self.task_sender.send(Task::Println(string)).unwrap()
+    }
+
+    pub fn open<F: FnOnce(File, Fs) + Send + 'static>(&self, path: &str, callback: F) {
+        self.task_sender.send(Task::Open(path.to_string(), Box::new(callback), self.clone())).unwrap()
+    }
+
+    pub fn read_to_string<F: FnOnce(String, Fs) + Send + 'static>(&self, file: File, callback: F) {
+        self.task_sender.send(Task::ReadToString(file, Box::new(callback), self.clone())).unwrap()
+    }
+
+    pub fn close(&self) {
+        self.task_sender.send(Task::Exit).unwrap()
+    }
+}
+
+type FileCallback = Box<FnBox(File, Fs) + Send>;
+type StringCallback = Box<FnBox(String, Fs) + Send>;
+
+pub enum Task {
+    Exit,
+    Println(String),
+    Open(String, FileCallback, Fs),
+    ReadToString(File, StringCallback, Fs),
+}
+
+pub enum TaskResult {
+    Exit,
+    Open(File, FileCallback, Fs),
+    ReadToString(String, StringCallback, Fs),
+}
+
+const TEST_FILE_VALUE: &str = "Hello, World!";
+
+#[test]
+fn test_fs_mio() {
+    let fs = Fs::new();
+    fs.open("./src/test.txt", |file, fs| {
+        fs.read_to_string(file, |value, fs| {
+            assert_eq!(TEST_FILE_VALUE, &value);
+            fs.println(value);
+            fs.close();
+        })
+    });
+}
--- src/tcp.rs
+++ src/tcp.rs
@@ -1,96 +1,96 @@
-use super::fs::Fs;
-use mio::*;
-use mio::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
-use std::time::{Duration, Instant};
-
-const SERVER_ACCEPT: Token = Token(0);
-const SERVER: Token = Token(1);
-const CLIENT: Token = Token(2);
-const SERVER_HELLO: &[u8] = b"PING";
-const CLIENT_HELLO: &[u8] = b"PONG";
-
-#[test]
-fn tcp_test() {
-    let addr = "127.0.0.1:13265".parse().unwrap();
-
-// Setup the server socket
-    let server = TcpListener::bind(&addr).unwrap();
-
-// Create a poll instance
-    let poll = Poll::new().unwrap();
-
-// Start listening for incoming connections
-    poll.register(&server, SERVER_ACCEPT, Ready::readable(),
-                  PollOpt::edge()).unwrap();
-
-// Setup the client socket
-    let mut client = TcpStream::connect(&addr).unwrap();
-
-    let mut server_handler = None;
-
-// Register the client
-    poll.register(&client, CLIENT, Ready::readable() | Ready::writable(),
-                  PollOpt::edge()).unwrap();
-
-// Create storage for events
-    let mut events = Events::with_capacity(1024);
-
-    let start = Instant::now();
-    let timeout = Duration::from_millis(10);
-    let fs = Fs::new();
-    loop {
-        poll.poll(&mut events, None).unwrap();
-        for event in events.iter() {
-            if start.elapsed() >= timeout {
-                return;
-            }
-            match event.token() {
-                SERVER_ACCEPT => {
-                    let (handler, addr) = server.accept().unwrap();
-                    fs.println(format!("accept from addr: {}", &addr));
-                    poll.register(&handler, SERVER, Ready::readable() | Ready::writable(), PollOpt::edge()).unwrap();
-                    server_handler = Some(handler);
-                }
-
-                SERVER => {
-                    if event.readiness().is_writable() {
-                        if let Some(ref mut handler) = &mut server_handler {
-                            handler.write(SERVER_HELLO).unwrap();
-                            fs.println("server wrote".to_string());
-                        }
-                    }
-                    if event.readiness().is_readable() {
-                        let mut hello = [0; 4];
-                        if let Some(ref mut handler) = &mut server_handler {
-                            match handler.read_exact(&mut hello) {
-                                Ok(_) => {
-                                    assert_eq!(CLIENT_HELLO, &hello);
-                                    fs.println("server received".to_string());
-                                }
-                                _ => continue
-                            }
-                        }
-                    }
-                }
-                CLIENT => {
-                    if event.readiness().is_writable() {
-                        client.write(CLIENT_HELLO).unwrap();
-                        fs.println("client wrote".to_string());
-                    }
-                    if event.readiness().is_readable() {
-                        let mut hello = [0; 4];
-                        match client.read_exact(&mut hello) {
-                            Ok(_) => {
-                                assert_eq!(SERVER_HELLO, &hello);
-                                fs.println("client received".to_string());
-                            }
-                            _ => continue
-                        }
-                    }
-                }
-                _ => unreachable!(),
-            }
-        }
-    };
-}
+//use super::fs::Fs;
+//use mio::*;
+//use mio::net::{TcpListener, TcpStream};
+//use std::io::{Read, Write};
+//use std::time::{Duration, Instant};
+//
+//const SERVER_ACCEPT: Token = Token(0);
+//const SERVER: Token = Token(1);
+//const CLIENT: Token = Token(2);
+//const SERVER_HELLO: &[u8] = b"PING";
+//const CLIENT_HELLO: &[u8] = b"PONG";
+//
+//#[test]
+//fn tcp_test() {
+//    let addr = "127.0.0.1:13265".parse().unwrap();
+//
+//// Setup the server socket
+//    let server = TcpListener::bind(&addr).unwrap();
+//
+//// Create a poll instance
+//    let poll = Poll::new().unwrap();
+//
+//// Start listening for incoming connections
+//    poll.register(&server, SERVER_ACCEPT, Ready::readable(),
+//                  PollOpt::edge()).unwrap();
+//
+//// Setup the client socket
+//    let mut client = TcpStream::connect(&addr).unwrap();
+//
+//    let mut server_handler = None;
+//
+//// Register the client
+//    poll.register(&client, CLIENT, Ready::readable() | Ready::writable(),
+//                  PollOpt::edge()).unwrap();
+//
+//// Create storage for events
+//    let mut events = Events::with_capacity(1024);
+//
+//    let start = Instant::now();
+//    let timeout = Duration::from_millis(10);
+//    let fs = Fs::new();
+//    loop {
+//        poll.poll(&mut events, None).unwrap();
+//        for event in events.iter() {
+//            if start.elapsed() >= timeout {
+//                return;
+//            }
+//            match event.token() {
+//                SERVER_ACCEPT => {
+//                    let (handler, addr) = server.accept().unwrap();
+//                    fs.println(format!("accept from addr: {}", &addr));
+//                    poll.register(&handler, SERVER, Ready::readable() | Ready::writable(), PollOpt::edge()).unwrap();
+//                    server_handler = Some(handler);
+//                }
+//
+//                SERVER => {
+//                    if event.readiness().is_writable() {
+//                        if let Some(ref mut handler) = &mut server_handler {
+//                            handler.write(SERVER_HELLO).unwrap();
+//                            fs.println("server wrote".to_string());
+//                        }
+//                    }
+//                    if event.readiness().is_readable() {
+//                        let mut hello = [0; 4];
+//                        if let Some(ref mut handler) = &mut server_handler {
+//                            match handler.read_exact(&mut hello) {
+//                                Ok(_) => {
+//                                    assert_eq!(CLIENT_HELLO, &hello);
+//                                    fs.println("server received".to_string());
+//                                }
+//                                _ => continue
+//                            }
+//                        }
+//                    }
+//                }
+//                CLIENT => {
+//                    if event.readiness().is_writable() {
+//                        client.write(CLIENT_HELLO).unwrap();
+//                        fs.println("client wrote".to_string());
+//                    }
+//                    if event.readiness().is_readable() {
+//                        let mut hello = [0; 4];
+//                        match client.read_exact(&mut hello) {
+//                            Ok(_) => {
+//                                assert_eq!(SERVER_HELLO, &hello);
+//                                fs.println("client received".to_string());
+//                            }
+//                            _ => continue
+//                        }
+//                    }
+//                }
+//                _ => unreachable!(),
+//            }
+//        }
+//    };
+//}
