3f5d797315fac7164a819a533da315654a2bc4e2
Hexilee <hexileee@gmail.com>
2018-12-11 08:48:14 +0800

    uncomments all test

--- src/fs.rs
+++ src/fs.rs
@@ -1,127 +1,127 @@
-//use crossbeam_channel::{unbounded, Sender};
-//use std::fs::File;
-//use std::io::Read;
-//use std::boxed::FnBox;
-//use std::thread;
-//
-//#[derive(Clone)]
-//pub struct Fs {
-//    task_sender: Sender<Task>,
-//}
-//
-//pub struct FsHandler {
-//    io_worker: thread::JoinHandle<()>,
-//    executor: thread::JoinHandle<()>,
-//}
-//
-//pub fn fs_async() -> (Fs, FsHandler) {
-//    let (task_sender, task_receiver) = unbounded();
-//    let (result_sender, result_receiver) = unbounded();
-//    let io_worker = std::thread::spawn(move || {
-//        loop {
-//            match task_receiver.recv() {
-//                Ok(task) => {
-//                    match task {
-//                        Task::Println(ref string) => println!("{}", string),
-//                        Task::Open(path, callback, fs) => {
-//                            result_sender
-//                                .send(TaskResult::Open(File::open(path).unwrap(), callback, fs))
-//                                .unwrap();
-//                        }
-//                        Task::ReadToString(mut file, callback, fs) => {
-//                            let mut value = String::new();
-//                            file.read_to_string(&mut value).unwrap();
-//                            result_sender
-//                                .send(TaskResult::ReadToString(value, callback, fs))
-//                                .unwrap()
-//                        }
-//                        Task::Exit => {
-//                            result_sender
-//                                .send(TaskResult::Exit)
-//                                .unwrap();
-//                            return;
-//                        }
-//                    }
-//                }
-//                Err(_) => {
-//                    return;
-//                }
-//            }
-//        }
-//    });
-//    let executor = std::thread::spawn(move || {
-//        loop {
-//            match result_receiver.recv() {
-//                Ok(result) => {
-//                    match result {
-//                        TaskResult::ReadToString(value, callback, fs) => callback(value, fs),
-//                        TaskResult::Open(file, callback, fs) => callback(file, fs),
-//                        TaskResult::Exit => return
-//                    }
-//                }
-//                Err(_) => {
-//                    return;
-//                }
-//            }
-//        }
-//    });
-//
-//    (Fs { task_sender }, FsHandler { io_worker, executor })
-//}
-//
-//impl Fs {
-//    pub fn println(&self, string: String) {
-//        self.task_sender.send(Task::Println(string)).unwrap()
-//    }
-//
-//    pub fn open<F: FnOnce(File, Fs) + Send + 'static>(&self, path: &str, callback: F) {
-//        self.task_sender.send(Task::Open(path.to_string(), Box::new(callback), self.clone())).unwrap()
-//    }
-//
-//    pub fn read_to_string<F: FnOnce(String, Fs) + Send + 'static>(&self, file: File, callback: F) {
-//        self.task_sender.send(Task::ReadToString(file, Box::new(callback), self.clone())).unwrap()
-//    }
-//
-//    pub fn close(&self) {
-//        self.task_sender.send(Task::Exit).unwrap();
-//    }
-//}
-//
-//impl FsHandler {
-//    pub fn join(self) {
-//        self.io_worker.join().unwrap();
-//        self.executor.join().unwrap();
-//    }
-//}
-//
-//type FileCallback = Box<FnBox(File, Fs) + Send>;
-//type StringCallback = Box<FnBox(String, Fs) + Send>;
-//
-//pub enum Task {
-//    Exit,
-//    Println(String),
-//    Open(String, FileCallback, Fs),
-//    ReadToString(File, StringCallback, Fs),
-//}
-//
-//pub enum TaskResult {
-//    Exit,
-//    Open(File, FileCallback, Fs),
-//    ReadToString(String, StringCallback, Fs),
-//}
-//
-//
-//const TEST_FILE_VALUE: &str = "Hello, World!";
-//
-//#[test]
-//fn test_fs() {
-//    let (fs, fs_handler) = fs_async();
-//    fs.open("./src/test.txt", |file, fs| {
-//        fs.read_to_string(file, |value, fs| {
-//            assert_eq!(TEST_FILE_VALUE, &value);
-//            fs.println(value);
-//            fs.close();
-//        })
-//    });
-//    fs_handler.join();
-//}
+use crossbeam_channel::{unbounded, Sender};
+use std::fs::File;
+use std::io::Read;
+use std::boxed::FnBox;
+use std::thread;
+
+#[derive(Clone)]
+pub struct Fs {
+    task_sender: Sender<Task>,
+}
+
+pub struct FsHandler {
+    io_worker: thread::JoinHandle<()>,
+    executor: thread::JoinHandle<()>,
+}
+
+pub fn fs_async() -> (Fs, FsHandler) {
+    let (task_sender, task_receiver) = unbounded();
+    let (result_sender, result_receiver) = unbounded();
+    let io_worker = std::thread::spawn(move || {
+        loop {
+            match task_receiver.recv() {
+                Ok(task) => {
+                    match task {
+                        Task::Println(ref string) => println!("{}", string),
+                        Task::Open(path, callback, fs) => {
+                            result_sender
+                                .send(TaskResult::Open(File::open(path).unwrap(), callback, fs))
+                                .unwrap();
+                        }
+                        Task::ReadToString(mut file, callback, fs) => {
+                            let mut value = String::new();
+                            file.read_to_string(&mut value).unwrap();
+                            result_sender
+                                .send(TaskResult::ReadToString(value, callback, fs))
+                                .unwrap()
+                        }
+                        Task::Exit => {
+                            result_sender
+                                .send(TaskResult::Exit)
+                                .unwrap();
+                            return;
+                        }
+                    }
+                }
+                Err(_) => {
+                    return;
+                }
+            }
+        }
+    });
+    let executor = std::thread::spawn(move || {
+        loop {
+            match result_receiver.recv() {
+                Ok(result) => {
+                    match result {
+                        TaskResult::ReadToString(value, callback, fs) => callback(value, fs),
+                        TaskResult::Open(file, callback, fs) => callback(file, fs),
+                        TaskResult::Exit => return
+                    }
+                }
+                Err(_) => {
+                    return;
+                }
+            }
+        }
+    });
+
+    (Fs { task_sender }, FsHandler { io_worker, executor })
+}
+
+impl Fs {
+    pub fn println(&self, string: String) {
+        self.task_sender.send(Task::Println(string)).unwrap()
+    }
+
+    pub fn open<F: FnOnce(File, Fs) + Send + 'static>(&self, path: &str, callback: F) {
+        self.task_sender.send(Task::Open(path.to_string(), Box::new(callback), self.clone())).unwrap()
+    }
+
+    pub fn read_to_string<F: FnOnce(String, Fs) + Send + 'static>(&self, file: File, callback: F) {
+        self.task_sender.send(Task::ReadToString(file, Box::new(callback), self.clone())).unwrap()
+    }
+
+    pub fn close(&self) {
+        self.task_sender.send(Task::Exit).unwrap();
+    }
+}
+
+impl FsHandler {
+    pub fn join(self) {
+        self.io_worker.join().unwrap();
+        self.executor.join().unwrap();
+    }
+}
+
+type FileCallback = Box<FnBox(File, Fs) + Send>;
+type StringCallback = Box<FnBox(String, Fs) + Send>;
+
+pub enum Task {
+    Exit,
+    Println(String),
+    Open(String, FileCallback, Fs),
+    ReadToString(File, StringCallback, Fs),
+}
+
+pub enum TaskResult {
+    Exit,
+    Open(File, FileCallback, Fs),
+    ReadToString(String, StringCallback, Fs),
+}
+
+
+const TEST_FILE_VALUE: &str = "Hello, World!";
+
+#[test]
+fn test_fs() {
+    let (fs, fs_handler) = fs_async();
+    fs.open("./src/test.txt", |file, fs| {
+        fs.read_to_string(file, |value, fs| {
+            assert_eq!(TEST_FILE_VALUE, &value);
+            fs.println(value);
+            fs.close();
+        })
+    });
+    fs_handler.join();
+}
--- src/spurious_events.rs
+++ src/spurious_events.rs
@@ -1,63 +1,63 @@
-//use mio::*;
-//use std::time::Duration;
-//use crossbeam_channel::unbounded;
-//
-//const TOKEN: Token = Token(0);
-//
-//#[test]
-//fn test_spurious_events() {
-//    let poll = Poll::new().unwrap();
-//    let (registration, set_readiness) = Registration::new2();
-//    poll.register(&registration, TOKEN, Ready::readable(), PollOpt::edge()).unwrap();
-//    let (task_sender, task_receiver) = unbounded();
-//    std::thread::spawn(move || {
-//        let mut task_counter = 0;
-//        loop {
-//            match task_receiver.recv() {
-//                Ok(_) => {
-//                    task_counter += 1;
-//                    if task_counter > 50 {
-//                        break;
-//                    }
-//                    set_readiness.set_readiness(Ready::readable()).unwrap();
-//                }
-//
-//                Err(err) => {
-//                    println!("err: {}", err);
-//                    break;
-//                }
-//            }
-//        }
-//        println!("task counter: {}", task_counter);
-//    });
-//
-//    let mut events = Events::with_capacity(1024);
-//
-//    let mut total_event_counter = 0;
-//    let mut token_event_counter = 0;
-//    task_sender.send(()).unwrap();
-//    loop {
-//        match poll.poll(&mut events, Some(Duration::from_secs(1))) {
-//            Ok(n) => {
-//                if n == 0 {
-//                    // timeout
-//                    break;
-//                }
-//                for event in events.iter() {
-//                    total_event_counter += 1;
-//                    match event.token() {
-//                        TOKEN => {
-//                            token_event_counter += 1;
-//                            task_sender.send(()).unwrap();
-//                        }
-//
-//                        _ => unreachable!()
-//                    }
-//                }
-//            }
-//
-//            _ => unreachable!()
-//        }
-//    }
-//    println!("total_event_counter: {}, token_event_counter: {}", total_event_counter, token_event_counter);
-//}
+use mio::*;
+use std::time::Duration;
+use crossbeam_channel::unbounded;
+
+const TOKEN: Token = Token(0);
+
+#[test]
+fn test_spurious_events() {
+    let poll = Poll::new().unwrap();
+    let (registration, set_readiness) = Registration::new2();
+    poll.register(&registration, TOKEN, Ready::readable(), PollOpt::edge()).unwrap();
+    let (task_sender, task_receiver) = unbounded();
+    std::thread::spawn(move || {
+        let mut task_counter = 0;
+        loop {
+            match task_receiver.recv() {
+                Ok(_) => {
+                    task_counter += 1;
+                    if task_counter > 50 {
+                        break;
+                    }
+                    set_readiness.set_readiness(Ready::readable()).unwrap();
+                }
+
+                Err(err) => {
+                    println!("err: {}", err);
+                    break;
+                }
+            }
+        }
+        println!("task counter: {}", task_counter);
+    });
+
+    let mut events = Events::with_capacity(1024);
+
+    let mut total_event_counter = 0;
+    let mut token_event_counter = 0;
+    task_sender.send(()).unwrap();
+    loop {
+        match poll.poll(&mut events, Some(Duration::from_secs(1))) {
+            Ok(n) => {
+                if n == 0 {
+                    // timeout
+                    break;
+                }
+                for event in events.iter() {
+                    total_event_counter += 1;
+                    match event.token() {
+                        TOKEN => {
+                            token_event_counter += 1;
+                            task_sender.send(()).unwrap();
+                        }
+
+                        _ => unreachable!()
+                    }
+                }
+            }
+
+            _ => unreachable!()
+        }
+    }
+    println!("total_event_counter: {}, token_event_counter: {}", total_event_counter, token_event_counter);
+}
