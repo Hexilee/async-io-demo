1cad333b583a47d9247708ea89d93e3a6c5dada9
Hexilee <hexileee@gmail.com>
2018-12-11 10:43:07 +0800

    format example fs_mio

--- examples/fs_mio.rs
+++ examples/fs_mio.rs
@@ -1,12 +1,13 @@
 #![feature(fnbox)]
 
-use crossbeam_channel::{unbounded, Sender};
+use crossbeam_channel::{unbounded, Sender, TryRecvError};
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read};
 use std::boxed::FnBox;
-use mio::*;
 use std::thread;
+use failure::Error;
 use std::time::Duration;
+use mio::*;
 
 #[derive(Clone)]
 pub struct Fs {
@@ -14,8 +15,8 @@ pub struct Fs {
 }
 
 pub struct FsHandler {
-    io_worker: thread::JoinHandle<()>,
-    executor: thread::JoinHandle<()>,
+    io_worker: thread::JoinHandle<Result<(), Error>>,
+    executor: thread::JoinHandle<Result<(), Error>>,
 }
 
 const FS_TOKEN: Token = Token(0);
@@ -26,7 +27,7 @@ pub fn fs_async() -> (Fs, FsHandler) {
     let poll = Poll::new().unwrap();
     let (registration, set_readiness) = Registration::new2();
     poll.register(&registration, FS_TOKEN, Ready::readable(), PollOpt::oneshot()).unwrap();
-    let io_worker = thread::spawn(move || {
+    let io_worker = std::thread::spawn(move || {
         loop {
             match task_receiver.recv() {
                 Ok(task) => {
@@ -34,22 +35,20 @@ pub fn fs_async() -> (Fs, FsHandler) {
                         Task::Println(ref string) => println!("{}", string),
                         Task::Open(path, callback, fs) => {
                             result_sender
-                                .send(TaskResult::Open(File::open(path).unwrap(), callback, fs))
-                                .unwrap();
-                            set_readiness.set_readiness(Ready::readable()).unwrap();
+                                .send(TaskResult::Open(File::open(path)?, callback, fs))?;
+                            set_readiness.set_readiness(Ready::readable())?;
                         }
                         Task::ReadToString(mut file, callback, fs) => {
                             let mut value = String::new();
-                            file.read_to_string(&mut value).unwrap();
+                            file.read_to_string(&mut value)?;
                             result_sender
-                                .send(TaskResult::ReadToString(value, callback, fs))
-                                .unwrap();
-                            set_readiness.set_readiness(Ready::readable()).unwrap();
+                                .send(TaskResult::ReadToString(value, callback, fs))?;
+                            set_readiness.set_readiness(Ready::readable())?;
                         }
                         Task::Exit => {
                             result_sender
-                                .send(TaskResult::Exit)
-                                .unwrap();
+                                .send(TaskResult::Exit)?;
+                            set_readiness.set_readiness(Ready::readable())?;
                             break;
                         }
                     }
@@ -58,13 +57,14 @@ pub fn fs_async() -> (Fs, FsHandler) {
                     break;
                 }
             }
-        };
+        }
+        Ok(())
     });
 
     let executor = thread::spawn(move || {
         let mut events = Events::with_capacity(1024);
         'outer: loop {
-            poll.poll(&mut events, Some(Duration::from_secs(1))).unwrap();
+            poll.poll(&mut events, Some(Duration::from_secs(1)))?;
             for event in events.iter() {
                 match event.token() {
                     FS_TOKEN => {
@@ -72,54 +72,59 @@ pub fn fs_async() -> (Fs, FsHandler) {
                             match result_receiver.try_recv() {
                                 Ok(result) => {
                                     match result {
-                                        TaskResult::ReadToString(value, callback, fs) => callback(value, fs),
-                                        TaskResult::Open(file, callback, fs) => callback(file, fs),
+                                        TaskResult::ReadToString(value, callback, fs) => callback.call_box((value, fs))?,
+                                        TaskResult::Open(file, callback, fs) => callback.call_box((file, fs))?,
                                         TaskResult::Exit => break 'outer
                                     }
                                 }
-                                Err(_) => {
-                                    break;
+                                Err(e) => {
+                                    match e {
+                                        TryRecvError::Empty => break,
+                                        TryRecvError::Disconnected => Err(e)?
+                                    }
                                 }
                             }
                         }
-                        poll.reregister(&registration, FS_TOKEN, Ready::readable(), PollOpt::oneshot()).unwrap();
+                        poll.reregister(&registration, FS_TOKEN, Ready::readable(), PollOpt::oneshot())?;
                     }
-
                     _ => unreachable!()
                 }
             }
         };
+        Ok(())
     });
     (Fs { task_sender }, FsHandler { io_worker, executor })
 }
 
 impl Fs {
-    pub fn println(&self, string: String) {
-        self.task_sender.send(Task::Println(string)).unwrap()
+    pub fn println(&self, string: String) -> Result<(), Error> {
+        Ok(self.task_sender.send(Task::Println(string))?)
     }
 
-    pub fn open<F: FnOnce(File, Fs) + Send + 'static>(&self, path: &str, callback: F) {
-        self.task_sender.send(Task::Open(path.to_string(), Box::new(callback), self.clone())).unwrap()
+    pub fn open<F>(&self, path: &str, callback: F) -> Result<(), Error>
+        where F: FnOnce(File, Fs) -> Result<(), Error> + Sync + Send + 'static {
+        Ok(self.task_sender.send(Task::Open(path.to_string(), Box::new(callback), self.clone()))?)
     }
 
-    pub fn read_to_string<F: FnOnce(String, Fs) + Send + 'static>(&self, file: File, callback: F) {
-        self.task_sender.send(Task::ReadToString(file, Box::new(callback), self.clone())).unwrap()
+    pub fn read_to_string<F>(&self, file: File, callback: F) -> Result<(), Error>
+        where F: FnOnce(String, Fs) -> Result<(), Error> + Sync + Send + 'static {
+        Ok(self.task_sender.send(Task::ReadToString(file, Box::new(callback), self.clone()))?)
     }
 
-    pub fn close(&self) {
-        self.task_sender.send(Task::Exit).unwrap()
+    pub fn close(&self) -> Result<(), Error> {
+        Ok(self.task_sender.send(Task::Exit)?)
     }
 }
 
 impl FsHandler {
-    pub fn join(self) {
-        self.io_worker.join().unwrap();
-        self.executor.join().unwrap();
+    pub fn join(self) -> Result<(), Error> {
+        self.io_worker.join().unwrap()?;
+        self.executor.join().unwrap()
     }
 }
 
-type FileCallback = Box<FnBox(File, Fs) + Send>;
-type StringCallback = Box<FnBox(String, Fs) + Send>;
+type FileCallback = Box<FnBox(File, Fs) -> Result<(), Error> + Sync + Send>;
+type StringCallback = Box<FnBox(String, Fs) -> Result<(), Error> + Sync + Send>;
 
 pub enum Task {
     Exit,
@@ -134,16 +139,18 @@ pub enum TaskResult {
     ReadToString(String, StringCallback, Fs),
 }
 
+
 const TEST_FILE_VALUE: &str = "Hello, World!";
 
-fn main() {
+fn main() -> Result<(), Error> {
     let (fs, fs_handler) = fs_async();
     fs.open("./examples/test.txt", |file, fs| {
         fs.read_to_string(file, |value, fs| {
             assert_eq!(TEST_FILE_VALUE, &value);
-            fs.println(value);
-            fs.close();
+            fs.println(value)?;
+            fs.close()
         })
-    });
-    fs_handler.join();
+    })?;
+    fs_handler.join()?;
+    Ok(())
 }
