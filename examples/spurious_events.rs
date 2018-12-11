f3760c1de5b27c41bdbdc9de4f143ca8ef5e932f
Hexilee <hexileee@gmail.com>
2018-12-11 09:09:39 +0800

    format example spurious_events

--- examples/spurious_events.rs
+++ examples/spurious_events.rs
@@ -1,15 +1,17 @@
 use mio::*;
 use std::time::Duration;
 use crossbeam_channel::unbounded;
+use std::io;
+use std::error::Error;
 
 const TOKEN: Token = Token(0);
 
-fn main() {
-    let poll = Poll::new().unwrap();
+fn main() -> Result<(), Box<Error>> {
+    let poll = Poll::new()?;
     let (registration, set_readiness) = Registration::new2();
-    poll.register(&registration, TOKEN, Ready::readable(), PollOpt::edge()).unwrap();
+    poll.register(&registration, TOKEN, Ready::readable(), PollOpt::edge())?;
     let (task_sender, task_receiver) = unbounded();
-    std::thread::spawn(move || {
+    let worker_handler = std::thread::spawn(move || -> io::Result<()> {
         let mut task_counter = 0;
         loop {
             match task_receiver.recv() {
@@ -18,7 +20,7 @@ fn main() {
                     if task_counter > 50 {
                         break;
                     }
-                    set_readiness.set_readiness(Ready::readable()).unwrap();
+                    set_readiness.set_readiness(Ready::readable())?;
                 }
 
                 Err(err) => {
@@ -28,13 +30,14 @@ fn main() {
             }
         }
         println!("task counter: {}", task_counter);
+        Ok(())
     });
 
     let mut events = Events::with_capacity(1024);
 
     let mut total_event_counter = 0;
     let mut token_event_counter = 0;
-    task_sender.send(()).unwrap();
+    task_sender.send(())?;
     loop {
         match poll.poll(&mut events, Some(Duration::from_secs(1))) {
             Ok(n) => {
@@ -47,7 +50,7 @@ fn main() {
                     match event.token() {
                         TOKEN => {
                             token_event_counter += 1;
-                            task_sender.send(()).unwrap();
+                            task_sender.send(())?;
                         }
 
                         _ => unreachable!()
@@ -58,5 +61,7 @@ fn main() {
             _ => unreachable!()
         }
     }
+    worker_handler.join().unwrap()?;
     println!("total_event_counter: {}, token_event_counter: {}", total_event_counter, token_event_counter);
+    Ok(())
 }
