a8893da4004333cba372cd495ce5a92da293e572
Hexilee <hexileee@gmail.com>
2018-12-15 05:50:41 +0800

    format code

--- examples/executor.rs
+++ examples/executor.rs
@@ -450,16 +450,16 @@ fn main() {
     env_logger::init();
     block_on(async {
         let mut listener = TcpListener::bind(&"127.0.0.1:7878".parse().unwrap()).expect("TcpListener bind fail");
-    info!("Listening on 127.0.0.1:7878");
-    while let Ok((mut stream, addr)) = await!(listener.accept()) {
-        info!("connection from {}", addr);
-        spawn(async move {
-            let client_hello = await!(stream.read()).unwrap();
-            let read_length = client_hello.len();
-            let write_length = await!(stream.write(client_hello)).unwrap();
-            assert_eq!(read_length, write_length);
-            stream.close();
-        });
-    }
-})
+        info!("Listening on 127.0.0.1:7878");
+        while let Ok((mut stream, addr)) = await!(listener.accept()) {
+            info!("connection from {}", addr);
+            spawn(async move {
+                let client_hello = await!(stream.read()).unwrap();
+                let read_length = client_hello.len();
+                let write_length = await!(stream.write(client_hello)).unwrap();
+                assert_eq!(read_length, write_length);
+                stream.close();
+            });
+        }
+    })
 }
