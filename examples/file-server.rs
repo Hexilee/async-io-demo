840406f6b7ea893dffc17cc3995b5777de05fefd
Hexilee <hexileee@gmail.com>
2018-12-23 19:43:47 +0800

    ready to change signature of spawn

--- examples/file-server.rs
+++ examples/file-server.rs
@@ -1,35 +1,39 @@
 #![feature(async_await)]
 #![feature(await_macro)]
+#![feature(futures_api)]
 
 #[macro_use]
 extern crate log;
 
-use asyncio::executor::{block_on, spawn, TcpListener};
+use asyncio::executor::{block_on, spawn, TcpListener, TcpStream};
 use asyncio::fs_future::{read_to_string};
 use failure::Error;
 
 fn main() -> Result<(), Error> {
     env_logger::init();
-    block_on(
-        async {
-            let mut listener = TcpListener::bind(&"127.0.0.1:7878".parse().unwrap())
-                .expect("TcpListener bind fail");
-            info!("Listening on 127.0.0.1:7878");
-            while let Ok((mut stream, addr)) = await!(listener.accept()) {
-                info!("connection from {}", addr);
-                spawn(
-                    async move {
-                        await!(stream.write_str("Please enter filename: ")).expect("write to stream fail");
-                        let file_name_vec = await!(stream.read()).expect("read from stream fail");
-                        let CRLF: &[char] = &['\r', '\n'];
-                        let file_name = String::from_utf8(file_name_vec).unwrap().trim_matches(CRLF).to_owned();
-                        let file_contents = await!(read_to_string(file_name)).expect("read to string from file fail");
-                        await!(stream.write_str(&file_contents)).expect("write file contents to stream fail");
-                        stream.close();
-                    },
-                )
-                .expect("spawn stream fail");
-            }
-        },
-    )
+    block_on(new_server())?;
+    Ok(())
+}
+
+
+const CRLF: &[char] = &['\r', '\n'];
+
+async fn new_server() -> Result<(), Error> {
+    let mut listener = TcpListener::bind(&"127.0.0.1:7878".parse()?)?;
+    info!("Listening on 127.0.0.1:7878");
+    while let Ok((mut stream, addr)) = await!(listener.accept()) {
+        info!("connection from {}", addr);
+        spawn(handle_stream(stream))?;
+    }
+    Ok(())
+}
+
+async fn handle_stream(mut stream: TcpStream) -> Result<(), Error> {
+    await!(stream.write_str("Please enter filename: "))?;
+    let file_name_vec = await!(stream.read())?;
+    let file_name = String::from_utf8(file_name_vec)?.trim_matches(CRLF).to_owned();
+    let file_contents = await!(read_to_string(file_name))?;
+    await!(stream.write_str(&file_contents))?;
+    stream.close();
+    Ok(())
 }
