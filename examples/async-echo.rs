fe659566e3f9152b6205da425e6f807ac13a6930
Hexilee <hexileee@gmail.com>
2018-12-23 19:54:28 +0800

    using try in examples/async-echo

--- examples/async-echo.rs
+++ examples/async-echo.rs
@@ -11,23 +11,23 @@ fn main() -> Result<(), Error> {
     env_logger::init();
     block_on(
         async {
-            let mut listener = TcpListener::bind(&"127.0.0.1:7878".parse().unwrap())
-                .expect("TcpListener bind fail");
+            let mut listener = TcpListener::bind(&"127.0.0.1:7878".parse()?)?;
             info!("Listening on 127.0.0.1:7878");
             while let Ok((mut stream, addr)) = await!(listener.accept()) {
                 info!("connection from {}", addr);
                 spawn(
                     async move {
-                        let client_hello = await!(stream.read()).expect("read from stream fail");
+                        let client_hello = await!(stream.read())?;
                         let read_length = client_hello.len();
                         let write_length =
-                            await!(stream.write(client_hello)).expect("write to stream fail");
+                            await!(stream.write(client_hello))?;
                         assert_eq!(read_length, write_length);
                         stream.close();
+                        Ok(())
                     },
-                )
-                .expect("spawn stream fail");
-            }
+                )?;
+            };
+            Ok(())
         },
-    )
+    )?
 }
